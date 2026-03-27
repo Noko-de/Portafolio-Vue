b1654dba2e81c2fc0645e7bd9c153abae5b37281
Joaquin Basaure <jbasaure.cruz@gmail.com>
2026-03-27 17:57:45 -0300

    inclusion de adivina el

--- src/components/AppProyects.vue
+++ src/components/AppProyects.vue
@@ -13,15 +13,14 @@
                 aria-label="Slide 5"></button>
         </div>
         <div class="carousel-inner ">
-            <div class="carousel-item active ">
+            <div class="carousel-item active    ">
                 <div class="card glitch-card ">
-                    <img src="../assets/img/proyecto_clima.png" class="card__img d-block w-100 h-100 object-fit-cover ">
+                    <img src="../assets/img/adivina_juego_2.png" class="card__img d-block w-100 h-100 object-fit-cover " style="height: 320px; ">
 
-                    <div class="card__content ">
-                        <p class="card__title">App de CLima</p>
+                    <div class="card__content p-2 align-content-around px-5">
+                        <p class="card__title ">Adivina el</p>
                         <p class="card__description">
-                            Proyecto enfocado en el aprendisaje de uso de API, usando la API Open weather map y
-                            uso basico de JavaScript
+                            Juego web dinámico donde los usuarios adivinan videojuegos a partir de imágenes, combinando lógica, diseño interactivo y consumo de datos en tiempo real.
                         </p>
                         <a href="https://noko-de.github.io/weather-app/" target="_blank">
                             <button class="glow-btn">Ir al proyecto</button>
@@ -29,17 +28,19 @@
                     </div>
                 </div>
             </div>
-            <div class="carousel-item">
-                <div class="card">
-                    <img src="../assets/img/web_1.webp " class="d-block  w-100 h-100 object-fit-cover"
-                        viewBox="0 0 24 24">
-                    <path
-                        d="M20 5H4V19L13.2923 9.70649C13.6828 9.31595 14.3159 9.31591 14.7065 9.70641L20 15.0104V5ZM2 3.9934C2 3.44476 2.45531 3 2.9918 3H21.0082C21.556 3 22 3.44495 22 3.9934V20.0066C22 20.5552 21.5447 21 21.0082 21H2.9918C2.44405 21 2 20.5551 2 20.0066V3.9934ZM8 11C6.89543 11 6 10.1046 6 9C6 7.89543 6.89543 7 8 7C9.10457 7 10 7.89543 10 9C10 10.1046 9.10457 11 8 11Z">
-                    </path></img>
-                    <div class="card__content ">
-                        <p class="card__title">Card Title</p>
-                        <p class="card__description">Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+            <div class="carousel-item  ">
+                <div class="card glitch-card ">
+                    <img src="../assets/img/proyecto_clima.png" class="card__img d-block w-100 h-100 object-fit-cover ">
+
+                    <div class="card__content p-2 align-content-around px-5 ">
+                        <p class="card__title">App de CLima</p>
+                        <p class="card__description ">
+                            Proyecto enfocado en el aprendizaje de uso de API, usando la API Open weather map y
+                            uso básico de JavaScript
                         </p>
+                        <a href="https://noko-de.github.io/weather-app/" target="_blank">
+                            <button class="glow-btn">Ir al proyecto</button>
+                        </a>
                     </div>
                 </div>
             </div>
